1ae7b22be55c453c1f224f25fba22aa63f37b783
ThatGuyJamal <jamallyons8@gmail.com>
2021-09-25 01:00:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import GitHub from "gh-pages"
+const GitHub = require("gh-pages")
 
 let site = "https://github.com/ThatGuyJamal/sneaky.css"
 
@@ -6,13 +6,13 @@ let site = "https://github.com/ThatGuyJamal/sneaky.css"
  * A simple function to deploy the static site to github pages
  */
 async function deploy() {
-	await GitHub.publish("./index.html", {
+	await GitHub.publish("./docs", {
 		branch: "website",
 		repo: site,
 		message: "Sneaky.css has been updated!",
 	})
 	await console.log(
-		`The Website has been push to github! View it live here: ${site}`
+		`The Website has been push to github! View it live here: https://thatguyjamal.github.io/sneaky.css/`
 	)
 }
 
