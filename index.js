735d961eda07a97a3a42439d9586a58bb3670794
ThatGuyJamal <jamallyons8@gmail.com>
2021-09-25 01:16:38 -0400

    fix: small html bugs

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const GitHub = require("gh-pages")
+const GitHub = require("gh-pages");
 
-let site = "https://github.com/ThatGuyJamal/sneaky.css"
+let site = "https://github.com/ThatGuyJamal/sneaky.css";
 
 /**
  * A simple function to deploy the static site to github pages
  */
 async function deploy() {
-	await GitHub.publish("./static", {
-		branch: "website",
-		repo: site,
-		message: "Sneaky.css has been updated!",
-	})
-	await console.log(
-		`The Website has been push to github! View it live here: https://thatguyjamal.github.io/sneaky.css/`
-	)
+  await GitHub.publish("./static", {
+    branch: "website",
+    repo: site,
+    message: "Sneaky.css has been updated!",
+  });
+  await console.log(
+    `The Website has been pushed to github! View it live here: https://thatguyjamal.github.io/sneaky.css/`
+  );
 }
 
-deploy()
+deploy();
